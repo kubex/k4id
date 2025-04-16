9f454b4502f345278535b22ae398b9070cc414be
Brooke Bryan <brooke@bajb.net>
2025-04-16 11:24:02 +0100

    Encode sha rather than compacting

--- hash.go
+++ hash.go
@@ -1,14 +1,13 @@
 package k4id
 
 import (
-	"encoding/hex"
 	"golang.org/x/crypto/sha3"
 )
 
 func Hashed(input string, length uint8) ID {
 	h := make([]byte, length+10)
 	sha3.ShakeSum256(h, []byte(input))
-	i := ID{uniqueKey: Base62.CompactHex(hex.EncodeToString(h))[:length-checksumSize]}
+	i := ID{uniqueKey: Base62.Encode(h)[:length-checksumSize]}
 	i.verification = i.checkSum(i.uniqueKey)
 	return i
 }
